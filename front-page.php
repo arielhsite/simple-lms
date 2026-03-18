5815a33807c548eb7ab9a8a28b186a859ac8d219
Ariel Habawel <arielhsite@gmail.com>
2026-03-18 12:04:55 +0800

    Initial commit - Simple LMS

---
+++ front-page.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Simple LMS - Front Page</title>
+</head>
+<body>
+<header>
+	<?php get_header(); ?>
+</header>
+<p>This is the front page of our theme</p>
+
+<footer>
+	<?php get_footer(); ?>
+</footer>
+</body>
+
+</html>
